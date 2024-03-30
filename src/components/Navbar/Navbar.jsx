0bedc1df04556bf1cb8c130381d5bc98a54884bb
morsalinamunmun <morsalinamunmun@gmail.com>
2024-03-31 02:23:39 +0600

    navbar created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const menus = <>
+              <li><NavLink to='/' className="hover:text-sky-600 border-b-2 focus:text-sky-600 focus:border-sky-600 pr-2">Home</NavLink></li>
+              <li><NavLink to='/dashboard' className="hover:text-sky-600 border-b-2 focus:text-sky-600 focus:border-sky-600 pr-2">Dashboard</NavLink></li>
+              <li><NavLink to='/fqa' className="hover:text-sky-600 border-b-2 focus:text-sky-600 focus:border-sky-600 pr-2">F</NavLink></li>
+              <li><NavLink to='/allTask' className="hover:text-sky-600 border-b-2 focus:text-sky-600 focus:border-sky-600 pr-2">Task List</NavLink></li>
+              {/* <li><NavLink to='/login' className='border-orange-500 rounded-lg'>Log In</NavLink></li> */}
+    </>
+
+    return (
+        <div className="navbar bg-base-100 ">
+            <div className="lg:w-[30%]">
+                <div className="dropdown mr-10 md:mr-40 lg:mr-0">
+                    <label tabIndex={0} className=" lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {menus}                    
+                    </ul>
+                </div>
+                {/* <img className="w-14" src={logo} alt="" /> */}
+                <Link to='/' className="ml-3 font-semibold normal-case text-xl"><span className="text-orange-500">SCC </span>Task Management</Link>
+            </div>
+            <div className="w-[70%] lg:ml-64 hidden lg:flex">
+                <ul className="menu-horizontal px-1 gap-2">
+                    {menus}
+                </ul>  
+            </div>
+            {/* {
+                    user ? 
+                    <>
+                         <div className="dropdown dropdown-end">
+                            <label   tabIndex ={0} className="btn btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src={user?.photoURL} alt="" />
+                                </div>
+                            </label>
+                            <ul tabIndex={0} className="p-2 shadow menu menu-sm dropdown-content z-[1] bg-base-100 rounded-box w-36 text-orange-500">
+                                <li className="font-semibold text-center mb-2">{user?.displayName}</li>
+                                <Link to='/dashboard'>Dashboard</Link>
+                                <div className="divider"></div>
+                                <li><a onClick={handleLogOut} className="font-semibold btn border-0 bg-orange-500 text-white">Sign Out</a></li>
+                            </ul>
+                        </div>                       
+                    </> : 
+                    <Link to='/login' className= "font-semibold btn border-0 bg-orang-500 text-white">Log In</Link>
+            } */}
+            <Link to='/login' className= "font-semibold btn border-0 bg-orang-500 text-white">Log In</Link>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './pages/Main/Layout';
+import Home from './pages/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
