c87c6cefd13d2a33fa14317fe4ca4cdc71b30374
morsalinamunmun <morsalinamunmun@gmail.com>
2024-03-31 14:07:46 +0600

    banner some change

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,13 +4,13 @@ const Banner = () => {
     return (
         <div className="min-h-screen">
             <div className="flex justify-between">
-                <div className="w-[50%] pt-[10%]">
+                <div className="md:w-[50%] pt-[11%] text-center md:text-left px-5">
                     <h1 className="text-5xl font-bold">Manage your task easily with simple.</h1>
-                    <p className="py-6"></p>
+                    <p className="py-6 text-xl">Task management services by CSq task manager</p>
                     <Link to="/login" className="bg-sky-600 text-white p-2 rounded">Get Started</Link>
                 </div>
-                <div className="absolute w-[50%] bg-gradient-to-r from-sky-300 to-sky-600 rounded-l-full ml-[11.5rem] py-52 left-[35%]">
-                    <img src="https://i.ibb.co/8ztGvYS/Chikitsha-Hub-hilarious-wisp-3febc2-netlify-app.png" className="w-[90%] relative right-48 rounded-lg shadow-2xl" />
+                <div className="md:absolute hidden md:block md:w-[50%] md:bg-gradient-to-r md:from-sky-300 md:to-sky-600 md:rounded-l-full md:ml-[11.5rem] md:py-48 md:left-[35%]">
+                    <img src="https://i.ibb.co/8ztGvYS/Chikitsha-Hub-hilarious-wisp-3febc2-netlify-app.png" className="w-[90%] relative right-40 rounded-lg shadow-2xl" />
                 </div>
             </div>
         </div>
